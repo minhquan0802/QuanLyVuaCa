822efcd1cfd45728ccc7725611ba2a2d133bc19a
Nguyễn Hồng Minh Quân <nguyenhongquan20042005@gmail.com>
2025-11-19 22:56:34 +0700

    init project

---
+++ src/main/java/com/minhquan/QuanLyVuaCa/QuanLyVuaCaApplication.java
@@ -0,0 +1,13 @@
+package com.minhquan.QuanLyVuaCa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuanLyVuaCaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuanLyVuaCaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/minhquan/QuanLyVuaCa/QuanLyVuaCaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.minhquan.QuanLyVuaCa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuanLyVuaCaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
