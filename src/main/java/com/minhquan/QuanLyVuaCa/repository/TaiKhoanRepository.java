bc38b1c3eb7561eb10c6fa120e705a02d32610d3
Nguyễn Hồng Minh Quân <nguyenhongquan20042005@gmail.com>
2025-11-20 22:36:08 +0700

    Fix database mapping but still not working, bruh

--- src/main/java/com/minhquan/QuanLyVuaCa/repository/TaiKhoanRepository.java
+++ src/main/java/com/minhquan/QuanLyVuaCa/repository/TaiKhoanRepository.java
@@ -1,6 +1,6 @@
 package com.minhquan.QuanLyVuaCa.repository;
 
-import com.minhquan.QuanLyVuaCa.entity.TaiKhoan;
+import com.minhquan.QuanLyVuaCa.Tam.TaiKhoan;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
--- src/main/java/com/minhquan/QuanLyVuaCa/service/TaiKhoanService.java
+++ src/main/java/com/minhquan/QuanLyVuaCa/service/TaiKhoanService.java
@@ -1,7 +1,7 @@
 package com.minhquan.QuanLyVuaCa.service;
 
 import com.minhquan.QuanLyVuaCa.dto.request.TaiKhoanCreationRequest;
-import com.minhquan.QuanLyVuaCa.entity.TaiKhoan;
+import com.minhquan.QuanLyVuaCa.Tam.TaiKhoan;
 import com.minhquan.QuanLyVuaCa.repository.TaiKhoanRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
