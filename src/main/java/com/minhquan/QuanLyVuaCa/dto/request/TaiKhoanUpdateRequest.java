46a9063714d76e110e3a746eaa75c52ea960dde0
Nguyen Hong Minh Quan <nguyenhongquan20042005@gmail.com>
2025-11-22 08:37:37 +0700

    Add CRUD for TaiKhoanController.java

---
+++ src/main/java/com/minhquan/QuanLyVuaCa/dto/request/TaiKhoanUpdateRequest.java
@@ -0,0 +1,13 @@
+package com.minhquan.QuanLyVuaCa.dto.request;
+
+import lombok.Data;
+
+@Data
+public class TaiKhoanUpdateRequest {
+    private String ho;
+    private String ten;
+    private String matkhau;
+    private String email;
+    private String sodienthoai;
+    private String diachi;
+}
