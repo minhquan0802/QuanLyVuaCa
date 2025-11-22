3a6989322b1db0850544f24f476e9cc69f169b63
Nguyen Hong Minh Quan <nguyenhongquan20042005@gmail.com>
2025-11-22 08:37:37 +0700

    Add CRUD for TaiKhoanController.java

--- src/main/java/com/minhquan/QuanLyVuaCa/controller/TaiKhoanController.java
+++ src/main/java/com/minhquan/QuanLyVuaCa/controller/TaiKhoanController.java
@@ -1,13 +1,14 @@
 package com.minhquan.QuanLyVuaCa.controller;
 
 import com.minhquan.QuanLyVuaCa.dto.request.TaiKhoanCreationRequest;
+import com.minhquan.QuanLyVuaCa.dto.request.TaiKhoanUpdateRequest;
 import com.minhquan.QuanLyVuaCa.entity.Taikhoan;
 import com.minhquan.QuanLyVuaCa.service.TaiKhoanService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 public class TaiKhoanController {
@@ -18,5 +19,23 @@ public class TaiKhoanController {
     Taikhoan taoUser(@RequestBody TaiKhoanCreationRequest request){
         Taikhoan newTaikhoan = taiKhoanService.taoTaiKhoan(request);
         return ResponseEntity.ok(newTaikhoan).getBody();
+    }
+
+    @GetMapping("/TaiKhoans")
+    List<Taikhoan> danhSachUser(){
+        return taiKhoanService.getTaiKhoans();
+    }
+    @GetMapping("/TaiKhoans/{idtaikhoan}")
+    Taikhoan timUser(@PathVariable("idtaikhoan") String idtaikhoan){
+        return taiKhoanService.timTaiKhoan(idtaikhoan);
+    }
+    @PutMapping("/TaiKhoans/{idtaikhoan}")
+    Taikhoan updateTK(@PathVariable("idtaikhoan") String idtaikhoan, @RequestBody TaiKhoanUpdateRequest request){
+        return taiKhoanService.updateTaiKhoan(idtaikhoan, request);
+    }
+    @DeleteMapping("/TaiKhoans/{idtaikhoan}")
+    String xoaTK(@PathVariable("idtaikhoan") String idtaikhoan){
+        taiKhoanService.xoaTaiKhoan(idtaikhoan);
+        return "Da xoa tai khoan";
     }
 }
--- src/main/java/com/minhquan/QuanLyVuaCa/service/TaiKhoanService.java
+++ src/main/java/com/minhquan/QuanLyVuaCa/service/TaiKhoanService.java
@@ -2,12 +2,15 @@ package com.minhquan.QuanLyVuaCa.service;
 
 import com.minhquan.QuanLyVuaCa.Enum.TrangThaiTaiKhoan;
 import com.minhquan.QuanLyVuaCa.dto.request.TaiKhoanCreationRequest;
+import com.minhquan.QuanLyVuaCa.dto.request.TaiKhoanUpdateRequest;
 import com.minhquan.QuanLyVuaCa.entity.Taikhoan;
 import com.minhquan.QuanLyVuaCa.entity.Vaitro;
 import com.minhquan.QuanLyVuaCa.repository.TaiKhoanRepository;
 import com.minhquan.QuanLyVuaCa.repository.VaitroRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
@@ -31,5 +34,26 @@ public class TaiKhoanService {
         taikhoan.setTrangthaitk(TrangThaiTaiKhoan.HOAT_DONG);
 
         return taiKhoanRepository.save(taikhoan);
+    }
+
+    public List<Taikhoan> getTaiKhoans(){
+        return taiKhoanRepository.findAll();
+    }
+    public Taikhoan timTaiKhoan(String id){
+        return taiKhoanRepository.findById(id).orElseThrow(()-> new RuntimeException("Khong tim thay user"));
+    }
+    public Taikhoan updateTaiKhoan(String idTaiKhoan,TaiKhoanUpdateRequest request){
+        Taikhoan taikhoan = timTaiKhoan(idTaiKhoan);
+        taikhoan.setHo(request.getHo());
+        taikhoan.setTen(request.getTen());
+        taikhoan.setEmail(request.getEmail());
+        taikhoan.setSodienthoai(request.getSodienthoai());
+        taikhoan.setDiachi(request.getDiachi());
+        taikhoan.setMatkhau(request.getMatkhau());
+
+        return taiKhoanRepository.save(taikhoan);
+    }
+    public void xoaTaiKhoan(String id){
+        taiKhoanRepository.deleteById(id);
     }
 }
