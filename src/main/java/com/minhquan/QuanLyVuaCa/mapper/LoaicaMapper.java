6131620a36f229fe0a350f9b6984ace28496d87b
Nguyễn Hồng Minh Quân <nguyenhongquan20042005@gmail.com>
2025-11-30 23:38:16 +0700

    FIx mapper but it's not error, just error IDE

--- src/main/java/com/minhquan/QuanLyVuaCa/mapper/LoaicaMapper.java
+++ src/main/java/com/minhquan/QuanLyVuaCa/mapper/LoaicaMapper.java
@@ -3,9 +3,7 @@ package com.minhquan.QuanLyVuaCa.mapper;
 import com.minhquan.QuanLyVuaCa.dto.request.LoaicaCeationRequest;
 import com.minhquan.QuanLyVuaCa.dto.request.LoaicaUpdateRequest;
 import com.minhquan.QuanLyVuaCa.dto.response.LoaicaResponse;
-import com.minhquan.QuanLyVuaCa.dto.response.TaikhoanResponse;
 import com.minhquan.QuanLyVuaCa.entity.Loaica;
-import com.minhquan.QuanLyVuaCa.entity.Taikhoan;
 import org.mapstruct.Mapper;
 import org.mapstruct.MappingTarget;
 
--- src/main/java/com/minhquan/QuanLyVuaCa/service/LoaicaService.java
+++ src/main/java/com/minhquan/QuanLyVuaCa/service/LoaicaService.java
@@ -14,6 +14,7 @@ import lombok.AccessLevel;
 import lombok.RequiredArgsConstructor;
 import lombok.experimental.FieldDefaults;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
